650084fdb4a381a98b287d1da2f4adc92f5c044f
Daniel Agudelo <daniel.agudelo5169@unaula.edu.co>
2020-08-16 16:41:42 -0500

    fixed punto_6

--- punto 6/punto_6.c
+++ punto 6/punto_6.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-#include <math.h>
+#define DIGITS 37 /* decimal places (not including the '2') */
+int main() {
+    int N = DIGITS+9, a[DIGITS+9], x = 0;
+    a[0] = 0;
+    a[1] = 2;
+    for (int n = 2; n < N; ++n) {
+        a[n] = 1;
+    }
+    for ( ; N > 9; --N) {
+        for (int n = N - 1; n > 0; --n) {
+            a[n] = x % n;
+            x = 10 * a[n-1] + x/n;
+        }
+        printf("%d", x);
+    }
 
-int main(){
-
-    printf("e: %.37f\n", M_E);
-
+    printf("\n");
     return 0;
 }
